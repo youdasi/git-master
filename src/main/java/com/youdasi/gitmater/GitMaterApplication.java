6e7843ba63909ca4f07223946cc30fbac7cb5a94
陈东 <chendong@cmiot.chinamobile.com>
2017-09-27 22:42:10 +0800

    项目主干

---
+++ src/main/java/com/youdasi/gitmater/GitMaterApplication.java
@@ -0,0 +1,12 @@
+package com.youdasi.gitmater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMaterApplication.class, args);
+	}
+}
