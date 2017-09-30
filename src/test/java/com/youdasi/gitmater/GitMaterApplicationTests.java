51f00c44248187074608ec5f773e80b88278707d
陈东 <chendong@cmiot.chinamobile.com>
2017-09-30 16:51:22 +0800

    找到Jgit作为开发框架

--- src/test/java/com/youdasi/gitmater/GitMaterApplicationTests.java
+++ src/test/java/com/youdasi/gitmater/GitMaterApplicationTests.java
@@ -1,9 +1,15 @@
 package com.youdasi.gitmater;
 
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +17,17 @@ public class GitMaterApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+
+	    try(Repository existingRepo = new FileRepositoryBuilder()
+                .setGitDir(new File("my_repo/.git"))
+                .build();){
+            Ref master = existingRepo.findRef("master");
+            // 获取该引用所指向的对象
+            ObjectId masterTip = master.getObjectId();
+        }catch (Exception e) {
+
+        }
+
+    }
 
 }
